ce3cf79731574b7f2480ec22f0a40cbc181834b9
Sara <saramesa@metricool.com>
2019-04-16 17:11:06 +0200

    config

--- src/api/bookCatalogApi.js
+++ src/api/bookCatalogApi.js
@@ -1,6 +1,8 @@
+import config from '../config/config';
+console.log(config.BASE_URL);
 export function getBooksCatalog() {
 	const API = 'https://cors-anywhere.herokuapp.com/https://saramesa-book-catalog.firebaseapp.com/api/catalog';
 	const BASE_URL = window.BASE_URL;
-  	return fetch(API)
+  	return fetch(BASE_URL)
     .then( (response) => response.json() );
 }
